8ad97daf9b2a01fa4b4c104330775dc9df92a0ca
Lara Oliveira <laraof.dev@gmail.com>
2022-11-10 12:23:36 -0300

    adiciona exercício semana-2 sobre markdown

---
+++ semana-2/readme.md
@@ -0,0 +1,11 @@
+## Sobre mim:
+
+**Nome:** Lara Oliveira Castilhos
+
+**Idade:** 28 anos
+
+**De onde sou:** Nasci e cresci em Belém do Pará, mas atualmente moro no Rio Grande do Sul
+
+**Hobbies:** Assistir dramas coreanos, tailandeses, chineses, japoneses (foco em bls)... Aprender novos idiomas (estou estudando coreano), assistir animes, creio que seja isso.
+
+**Profissão:** Atualmente estou desempregada, mas sou graduada em arquitetura e urbanismo e estou em transição de carreira para área tech, focada em desenvolvimento front-end. 
---
+++ semana-6/aula-8/script.js
@@ -0,0 +1,107 @@
+let produtos = [
+    {
+      nome: "TV 20 polegadas",
+      categoria: "áudio e vídeo",
+      preco: 1500
+    },
+    {
+      nome: "Mouse",
+      categoria: "informática",
+      preco: 70
+    },
+    {
+      nome: "Teclado",
+      categoria: "informática",
+      preco: 350 
+    },
+    {
+      nome: "Aparelho de som",
+      categoria: "áudio e vídeo",
+      preco: 600
+    },
+    {
+      nome: "Sanduicheira",
+      categoria: "eletrodomésticos",
+      preco: 130
+    },
+    {
+        nome: "Microondas 20L",
+        categoria: "eletrodomésticos",
+        preco: 800
+      },
+      {
+        nome: "Geladeira frost free",
+        categoria: "eletrodomésticos",
+        preco: 2100
+      },
+      {
+        nome: "Carregador de notebook Dell",
+        categoria: "informática",
+        preco: 140
+      },
+      {
+        nome: "Caixa de som JBL",
+        categoria: "áudio e vídeo",
+        preco: 230
+      },
+      {
+        nome: "Webcam",
+        categoria: "informática",
+        preco: 330
+      },
+      {
+        nome: "Fogão 4 bocas",
+        categoria: "eletrodomésticos",
+        preco: 680
+      },
+      {
+        nome: "Monitor 24 polegadas",
+        categoria: "informática",
+        preco: 900
+      },
+      {
+        nome: "Air fryer",
+        categoria: "eletrodomésticos",
+        preco: 450
+      }
+    ]
+  
+
+let btnInformatica = document.getElementById("informatica");
+let btnEletros = document.getElementById("eletros");
+let btnAV = document.getElementById("aV");
+
+function listarProdutos(listaProdutos) {
+
+    let section = document.querySelector('section');
+    let listaExistente = document.querySelectorAll('.produto');
+
+    if (listaExistente.length > 0) {
+        listaExistente.forEach(function (itemListaExistente) {
+            section.removeChild(itemListaExistente);
+        })
+    }
+
+    listaProdutos.forEach(function (listaPorCategoria) {
+        
+        let p = document.createElement('p');
+        p.classList.add('produto');
+        p.innerHTML = `Produto: ${listaPorCategoria.nome}, Valor: R$ ${listaPorCategoria.preco}`;
+        section.appendChild(p);
+    })
+
+}
+
+function filtrarProdutos(evento) {
+  let produtosFiltrados = [];
+  produtosFiltrados = produtos.filter(function (produto) {
+    return produto.categoria === evento.target.innerHTML;
+  })
+  listarProdutos(produtosFiltrados);
+}
+
+console.log (filtrarProdutos);
+
+btnInformatica.addEventListener('click', filtrarProdutos);
+btnEletros.addEventListener('click', filtrarProdutos);
+btnAV.addEventListener('click', filtrarProdutos);
